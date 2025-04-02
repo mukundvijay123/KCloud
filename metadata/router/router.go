b948bc079c1afb9dd04d14c3f3baa9b9afbb7e7a
mukundvijay123 <vijay.mukund7845@gmail.com>
2025-04-02 21:57:39 +0530

    Cors middleware added to metadata subrouter

--- metadata/router/router.go
+++ metadata/router/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/mukundvijay123/KCloud/metadata/middleware"
 	authentication "github.com/mukundvijay123/KCloud/metadata/middleware/auth"
 	"github.com/mukundvijay123/KCloud/metadata/services/metadata"
 	"github.com/mukundvijay123/KCloud/metadata/services/user"
@@ -25,6 +26,7 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+	subrouter.Use(middleware.CorsMiddleware)
 
 	metadataSubRouter := subrouter.PathPrefix("/resources").Subrouter()
 	metadataSubRouter.Use(authentication.JWTMiddleware)
